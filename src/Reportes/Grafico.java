81fc0c42c8880f418b5fe3b0c1b941c4767712c3
Ffreyja <tatianatsslow@gmail.com>
2023-05-31 11:00:32 -0500

    SISTEMA VENTA MYSQL

---
+++ src/Reportes/Grafico.java
@@ -0,0 +1,41 @@
+
+package Reportes;
+
+import Modelo.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+public class Grafico {
+    public static void Graficar(String fecha){
+        Connection con;
+        Conexion cn = new Conexion();
+        PreparedStatement ps;
+        ResultSet rs;
+        try {
+            String sql = "SELECT total FROM ventas WHERE fecha = ?";
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, fecha);
+            rs = ps.executeQuery();
+            DefaultPieDataset dateset = new DefaultPieDataset();
+            while(rs.next()){
+                dateset.setValue(rs.getString("total"), rs.getDouble("total"));
+            }
+            JFreeChart jf = ChartFactory.createPieChart("Reporte de Venta", dateset);
+            ChartFrame f = new ChartFrame("Total de Ventas por dia", jf);
+            //tamaño
+            f.setSize(1000, 500);
+            //centrar
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
